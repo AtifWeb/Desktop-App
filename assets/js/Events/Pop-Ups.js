9f2429c07af79f6fdb1d6a3e3aaf9c508bdb4012
AtifWeb <mytechlife96@gmail.com>
2021-01-30 08:52:47 -0800

    pop-ups

--- assets/js/Events/Pop-Ups.js
+++ assets/js/Events/Pop-Ups.js
@@ -1,5 +1,6 @@
 export class DashboardPopUps {
   constructor() {
+    self.popupsContainer = document.querySelector(".pop-ups");
     self.DashboardContainer = document.querySelector(".dashboard-container");
     self.CreateAccountForm = document.querySelector(".create-account-popup");
     self.AddGroupTaskForm = document.querySelector(".create-group-task-popup");
@@ -8,33 +9,33 @@ export class DashboardPopUps {
     self.CreateTaskPopUp = document.querySelector(".create-task-popup");
   }
   ShowCreateAccountPopUp() {
-    self.DashboardContainer.style.display = "none";
+    self.popupsContainer.style.display = "block";
     self.CreateAccountForm.style.display = "block";
   }
   ShowAddGroupTaskPopUp() {
-    self.DashboardContainer.style.display = "none";
+    self.popupsContainer.style.display = "block";
     self.AddGroupTaskForm.style.display = "flex";
   }
   ShowCaptchasPopUp() {
-    self.DashboardContainer.style.display = "none";
+    self.popupsContainer.style.display = "block";
     self.CaptchasPopUp.style.display = "block";
   }
 
   ShowCreateTaskPop() {
-    self.DashboardContainer.style.display = "none";
+    self.popupsContainer.style.display = "block";
     self.CreateTaskPopUp.style.display = "flex";
   }
 
   HideCreateAccountPopUp() {
-    self.DashboardContainer.style.display = "block";
+    self.popupsContainer.style.display = "none";
     self.CreateAccountForm.style.display = "none";
   }
   HideCaptchasPopUp() {
-    self.DashboardContainer.style.display = "block";
+    self.popupsContainer.style.display = "none";
     self.CaptchasPopUp.style.display = "none";
   }
   HideCreateTaskPop() {
-    self.DashboardContainer.style.display = "block";
+    self.popupsContainer.style.display = "none";
     self.CreateTaskPopUp.style.display = "none";
   }
 }
